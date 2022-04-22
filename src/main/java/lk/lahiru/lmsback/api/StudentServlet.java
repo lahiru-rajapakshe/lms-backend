c8d46300c021f99c78af2f01160a159e566544a1
LahiruRJ <sanlahiru720@gmail.com>
2022-04-22 12:16:05 +0530

    Add new ValidationException and validate request data

--- src/main/java/lk/lahiru/lmsback/api/StudentServlet.java
+++ src/main/java/lk/lahiru/lmsback/api/StudentServlet.java
@@ -83,12 +83,15 @@ package lk.lahiru.lmsback.api;
 import jakarta.json.*;
         import jakarta.json.bind.Jsonb;
         import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import lk.lahiru.lmsback.model.StudentDTO;
 import lk.lahiru.lmsback.util.Customer;
 
 
 import javax.servlet.*;
         import javax.servlet.http.*;
-        import java.io.BufferedReader;
+import javax.xml.bind.ValidationException;
+import java.io.BufferedReader;
         import java.io.IOException;
         import java.io.PrintWriter;
         import java.util.ArrayList;
@@ -111,6 +114,28 @@ public class StudentServlet extends HttpServlet {
             return;
         }
 
+        Jsonb jsonb = JsonbBuilder.create();
+        try {
+            StudentDTO student = jsonb.fromJson(req.getReader(), StudentDTO.class);
+
+            if (student.getName() == null || !student.getName().matches("[A-Za-z ]+")){
+                throw new ValidationException("Invalid student name");
+            }else if (student.getNic() == null || !student.getNic().matches("\\d{9}[Vv]")){
+                throw new ValidationException("Invalid student nic");
+            }else if (student.getEmail() == null || !student.getEmail().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
+                throw new ValidationException("Invalid student email");
+            }
+
+            System.out.println(student);
+        }catch (JsonbException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid JSON");
+        }catch (ValidationException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        }
+    }
+
+
+
 //        List<Customer> customerList = new ArrayList<>();
 
 //        JsonReader jsonReader = Json.createReader(req.getReader());
@@ -123,23 +148,23 @@ public class StudentServlet extends HttpServlet {
 //            customerList.add(new Customer(id, name, address));
 //        }
 
-        Jsonb jsonb = JsonbBuilder.create();
-        List<Customer> customerList = jsonb.fromJson(req.getReader(),
-                new ArrayList<Customer>(){}.getClass().getGenericSuperclass());
+//        Jsonb jsonb = JsonbBuilder.create();
+//        List<Customer> customerList = jsonb.fromJson(req.getReader(),
+//                new ArrayList<Customer>(){}.getClass().getGenericSuperclass());
 
-        customerList.forEach(System.out::println);
-
-        List<Customer> anotherCustomerList = new ArrayList<>();
-        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
-        anotherCustomerList.add(new Customer("C002", "Nuwan", "Galle"));
-        anotherCustomerList.add(new Customer("C003", "Nuwan", "Galle"));
-        anotherCustomerList.add(new Customer("C004", "Nuwan", "Galle"));
-        anotherCustomerList.add(new Customer("C005", "Nuwan", "Galle"));
-
-        resp.setContentType("application/json");
-        jsonb.toJson(anotherCustomerList, resp.getWriter());
-
-    }
+//        customerList.forEach(System.out::println);
+//
+//        List<Customer> anotherCustomerList = new ArrayList<>();
+//        anotherCustomerList.add(new Customer("C001", "Nuwan", "Galle"));
+//        anotherCustomerList.add(new Customer("C002", "Nuwan", "Galle"));
+//        anotherCustomerList.add(new Customer("C003", "Nuwan", "Galle"));
+//        anotherCustomerList.add(new Customer("C004", "Nuwan", "Galle"));
+//        anotherCustomerList.add(new Customer("C005", "Nuwan", "Galle"));
+//
+//        resp.setContentType("application/json");
+//        jsonb.toJson(anotherCustomerList, resp.getWriter());
+//
+//    }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
