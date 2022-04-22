61f0f2157d1b04261e25542165367c6220614e16
LahiruRJ <sanlahiru720@gmail.com>
2022-04-22 18:37:01 +0530

    Implement CoreFilter Class

--- src/main/java/lk/lahiru/lmsback/filter/CorsFilter.java
+++ src/main/java/lk/lahiru/lmsback/filter/CorsFilter.java
@@ -1,5 +1,25 @@
 package lk.lahiru.lmsback.filter;
 
-public class CorsFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebFilter(filterName = "CorsFilter", urlPatterns = "/*")
+public class CorsFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("Origin");
+        if (origin != null && origin.contains(getServletContext().getInitParameter("origin"))){
+            res.setHeader("Access-Control-Allow-Origin", origin);
+            if (req.getMethod().equals("OPTIONS")){
+                res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
+                res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+                res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+            }
+        }
+        chain.doFilter(req,res);
+    }
 }
